4a34dbb000a4b5e055cf18cbc18bb4a38eb77c17
Travis <travis.lee.white.6@gmail.com>
2021-02-15 12:19:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import successImg from './successImg.png';
+import headerImg from './headerImg.png';
+import slack from './slack.png';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={headerImg} className="headerImg" alt="logo" />
+        <img src={successImg}  alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Success! CatMe is installed to your workspace<br/>
+          Type the <code>/catme</code> command to get started
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Button
+              style={{backgroundColor: "#FFFFFF", color: "#000000"}}
+              onClick={() => window.open("slack://open", "_parent")}
+              startIcon={<img src={slack} style={{height: 24, width: 24}} alt="logo" />}
+              variant="contained"
+              color="primary"
+              component="span">
+            Open
+          </Button>
       </header>
     </div>
   );
